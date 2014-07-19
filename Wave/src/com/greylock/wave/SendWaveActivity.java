c14155f5c6ea7b6aca6ae2c7a8c3c3450903c239
Will Jamieson <wjamieson@pandora.com>
2014-07-19 12:35:48 -0700

    sendWave

---
+++ Wave/src/com/greylock/wave/SendWaveActivity.java
@@ -0,0 +1,15 @@
+package com.greylock.wave;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class SendWaveActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_send_wave);
+	}
+}
