2d836729e0924c036ab5249a71f58922213cecb2
Will Jamieson <wjamieson@pandora.com>
2014-07-19 16:48:50 -0700

    Adds location and map screen

--- Wave/src/com/greylock/wave/WaveMainTabsActivity.java
+++ Wave/src/com/greylock/wave/WaveMainTabsActivity.java
@@ -1,24 +1,41 @@
 package com.greylock.wave;
 
+import java.util.List;
 import java.util.Locale;
 
 import android.app.ActionBar;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.graphics.drawable.ColorDrawable;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.v13.app.FragmentPagerAdapter;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.Window;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.location.LocationClient;
 
 public class WaveMainTabsActivity extends Activity implements
-		ActionBar.TabListener {
+ActionBar.TabListener, 
+GooglePlayServicesClient.ConnectionCallbacks,
+GooglePlayServicesClient.OnConnectionFailedListener {
 
 	/**
 	 * The {@link android.support.v4.view.PagerAdapter} that will provide
@@ -28,11 +45,17 @@ public class WaveMainTabsActivity extends Activity implements
 	 * {@link android.support.v13.app.FragmentStatePagerAdapter}.
 	 */
 	SectionsPagerAdapter mSectionsPagerAdapter;
+	private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
 
 	/**
 	 * The {@link ViewPager} that will host the section contents.
 	 */
 	ViewPager mViewPager;
+	Location mCurrentLocation;
+	LocationClient mLocationClient;
+	GPSTracker gps;
+
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -45,10 +68,12 @@ public class WaveMainTabsActivity extends Activity implements
 
 		Resources resources = getResources();
 
-		actionBar.setBackgroundDrawable(new ColorDrawable(
-				resources.getColor(R.color.actionbar_background_color)));
-		actionBar.setStackedBackgroundDrawable(new ColorDrawable(
-				resources.getColor(R.color.actionbar_tab_background_color)));
+		actionBar.setBackgroundDrawable(new ColorDrawable(resources
+				.getColor(R.color.actionbar_background_color)));
+		actionBar.setStackedBackgroundDrawable(new ColorDrawable(resources
+				.getColor(R.color.actionbar_tab_background_color)));
+
+		actionBar.setHomeButtonEnabled(true);
 
 		// Create the adapter that will return a fragment for each of the three
 		// primary sections of the activity.
@@ -62,12 +87,12 @@ public class WaveMainTabsActivity extends Activity implements
 		// tab. We can also use ActionBar.Tab#select() to do this if we have
 		// a reference to the Tab.
 		mViewPager
-				.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
-					@Override
-					public void onPageSelected(int position) {
-						actionBar.setSelectedNavigationItem(position);
-					}
-				});
+		.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+			@Override
+			public void onPageSelected(int position) {
+				actionBar.setSelectedNavigationItem(position);
+			}
+		});
 
 		// For each of the sections in the app, add a tab to the action bar.
 		for (int i = 0; i < mSectionsPagerAdapter.getCount(); i++) {
@@ -79,6 +104,25 @@ public class WaveMainTabsActivity extends Activity implements
 					.setText(mSectionsPagerAdapter.getPageTitle(i))
 					.setTabListener(this));
 		}
+
+		mLocationClient = new LocationClient(this, this, this);
+
+	}
+
+	@Override
+	protected void onStart() {
+		super.onStart();
+		// Connect the client.
+		mLocationClient.connect();
+
+
+	}
+
+	@Override
+	protected void onStop() {
+		// Disconnecting the client invalidates it.
+		mLocationClient.disconnect();
+		super.onStop();
 	}
 
 	@Override
@@ -98,10 +142,14 @@ public class WaveMainTabsActivity extends Activity implements
 			startActivity(new Intent(this, RequestChannelActivity.class));
 			return true;
 		}
-		
+
 		if (id == R.id.manageLocation) {
 			startActivity(new Intent(this, LocationActivity.class));
 			return true;
+		}
+
+		if (id == android.R.id.home) {
+			startActivity(new Intent(this, SendWaveActivity.class));
 		}
 		return super.onOptionsItemSelected(item);
 	}
@@ -169,9 +217,159 @@ public class WaveMainTabsActivity extends Activity implements
 			case 2:
 				return getString(R.string.title_fragment_all_tab)
 						.toUpperCase(l);
-						
+
 			}
 			return null;
 		}
+	}
+
+	// Define a DialogFragment that displays the error dialog
+	public static class ErrorDialogFragment extends DialogFragment {
+		// Global field to contain the error dialog
+		private Dialog mDialog;
+		// Default constructor. Sets the dialog field to null
+		public ErrorDialogFragment() {
+			super();
+			mDialog = null;
+		}
+		// Set the dialog to display
+		public void setDialog(Dialog dialog) {
+			mDialog = dialog;
+		}
+		// Return a Dialog to the DialogFragment.
+		@Override
+		public Dialog onCreateDialog(Bundle savedInstanceState) {
+			return mDialog;
+		}
+	}
+	/*
+	 * Handle results returned to the FragmentActivity
+	 * by Google Play services
+	 */
+	@Override
+	protected void onActivityResult(
+			int requestCode, int resultCode, Intent data) {
+		// Decide what to do based on the original request code
+		switch (requestCode) {
+		case CONNECTION_FAILURE_RESOLUTION_REQUEST :
+			/*
+			 * If the result code is Activity.RESULT_OK, try
+			 * to connect again
+			 */
+			switch (resultCode) {
+			case Activity.RESULT_OK :
+				/*
+				 * Try the request again
+				 */
+				break;
+			}
+		}
+	}
+	private boolean servicesConnected() {
+		// Check that Google Play services is available
+		int resultCode =
+				GooglePlayServicesUtil.
+				isGooglePlayServicesAvailable(this);
+		// If Google Play services is available
+		if (ConnectionResult.SUCCESS == resultCode) {
+			// In debug mode, log the status
+			Log.d("Location Updates",
+					"Google Play services is available.");
+			// Continue
+			return true;
+			// Google Play services was not available for some reason
+		} else {
+			// Get the error code
+			int errorCode = 0;
+			// Get the error dialog from Google Play services
+			Dialog errorDialog = GooglePlayServicesUtil.getErrorDialog(
+					errorCode,
+					this,
+					CONNECTION_FAILURE_RESOLUTION_REQUEST);
+
+			// If Google Play services can provide an error dialog
+			if (errorDialog != null) {
+				// Create a new DialogFragment for the error dialog
+				ErrorDialogFragment errorFragment =
+						new ErrorDialogFragment();
+				// Set the dialog in the DialogFragment
+				errorFragment.setDialog(errorDialog);
+
+			}
+		}
+		return false;
+	}
+
+	@Override
+	public void onConnectionFailed(ConnectionResult arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onConnected(Bundle arg0) {
+		// TODO Auto-generated method stub
+		mCurrentLocation = mLocationClient.getLastLocation();
+		
+		if(mCurrentLocation== null)	{
+
+			showSettingsAlert();
+			
+			LocationManager manager = (LocationManager) getApplicationContext().getSystemService(Context.LOCATION_SERVICE);
+
+			Location location = null;
+
+			List<String> providers = manager.getProviders(true);
+
+			for(String provider : providers){
+
+			location = manager.getLastKnownLocation(provider);
+			//maybe try adding some Criteria here
+
+			if(location != null) 
+				mCurrentLocation = location;
+			}
+			if(mLocationClient.getLastLocation() != null)
+				mCurrentLocation = mLocationClient.getLastLocation();
+
+		}
+		
+		
+		//Toast.makeText(this, ""+ mCurrentLocation.getLatitude(), Toast.LENGTH_LONG).show();
+
+	}
+	
+	public void showSettingsAlert(){
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(WaveMainTabsActivity.this);
+      
+        // Setting Dialog Title
+        alertDialog.setTitle("GPS is settings");
+  
+        // Setting Dialog Message
+        alertDialog.setMessage("GPS is not enabled. Do you want to go to settings menu?");
+  
+        // On pressing Settings button
+        alertDialog.setPositiveButton("Settings", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog,int which) {
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                WaveMainTabsActivity.this.startActivity(intent);
+            }
+        });
+  
+        // on pressing cancel button
+        alertDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+            dialog.cancel();
+            }
+        });
+  
+        // Showing Alert Message
+        alertDialog.show();
+    }
+
+	@Override
+	public void onDisconnected() {
+		// TODO Auto-generated method stub
+
 	}
 }
